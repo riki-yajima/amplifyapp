1e1ea3362f8e39efb5e1bccbca14f93532f49909
riki-yajima <94480246+riki-yajima@users.noreply.github.com>
2022-04-25 15:05:08 +0900

    v2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header className="App-header">
+        <h1>Hello from V2</h1>
+      </header>
     </div>
   );
 }
